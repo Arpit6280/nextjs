21b1e065683add2b4c45ea72be2d76812a43a004
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-12-17 20:08:48 +0530

    Final touch to About US page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,23 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 function AboutUs() {
-  return <h1>About us page</h1>;
+  return (
+    <Fragment>
+      <h1>About us page</h1>;
+      <ul>
+        <li>
+          <Link href="/aboutus/1">Yash</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/2">Vaibhav</Link>
+        </li>
+        <li>
+          <Link href="/aboutus/3">Suresh</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default AboutUs;
